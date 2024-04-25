38130bc541fd0f94fbaf7c45285f926b4c9d157b
Arush Saxena <88478157+arushsaxena1998@users.noreply.github.com>
2024-04-25 13:04:46 +0000

    testing

--- tests/test.js
+++ tests/test.js
@@ -35,7 +35,7 @@ async function searchTextOnGoogle() {
     accessKey: KEY,
     name: "test session", // name of the test
     build: platform + browserName + version, // name of the build
-    "smartUI.project": "CI-Check-NBC",
+    "smartUI.project": "NBC-Storybook-222222",
     "smartUI.build": "[PR: 6208]",
     github: {
       url: process.env.GITHUB_URL,
